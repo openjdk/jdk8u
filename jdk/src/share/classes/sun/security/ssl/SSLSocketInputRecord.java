a8cc88528bdde96bf81f29b42125e715d8dfd209
Cole-Greer <coleg@bitquilltech.com>
2023-07-04 17:44:13 -0700

    Backport f07e69187b79863b78d5e985be84f15f4ecc462b

--- jdk/src/share/classes/sun/security/ssl/SSLSocketInputRecord.java
+++ jdk/src/share/classes/sun/security/ssl/SSLSocketInputRecord.java
@@ -257,7 +257,11 @@ final class SSLSocketInputRecord extends InputRecord implements SSLRecord {
         // Decrypt the fragment
         //
         ByteBuffer fragment;
+        recordLock.lock();
         try {
+            if (isClosed) {
+                return null;
+            }
             Plaintext plaintext =
                     readCipher.decrypt(contentType, destination, null);
             fragment = plaintext.fragment;
@@ -267,6 +271,8 @@ final class SSLSocketInputRecord extends InputRecord implements SSLRecord {
         } catch (GeneralSecurityException gse) {
             throw (SSLProtocolException)(new SSLProtocolException(
                     "Unexpected exception")).initCause(gse);
+        } finally {
+            recordLock.unlock();
         }
 
         if (contentType != ContentType.HANDSHAKE.id &&
