13446637c5b506e35f5d45165966fabd96737bed
Antonio Vieiro <avieirov@redhat.com>
2025-09-23 00:11:03 +0200

    Update test not to use PassFailJFrame not available in JDK8

--- jdk/test/javax/swing/JMenuItem/TestRadioAndCheckMenuItemWithIcon.java
+++ jdk/test/javax/swing/JMenuItem/TestRadioAndCheckMenuItemWithIcon.java
@@ -21,7 +21,7 @@
  * questions.
  */
 
-/*
+ /*
  * @test
  * @bug 8348760
  * @summary Verify if RadioButtonMenuItem bullet and
@@ -29,20 +29,25 @@
  *          JRadioButtonMenuItem and JCheckboxMenuItem
  *          is rendered with ImageIcon in WindowsLookAndFeel
  * @requires (os.family == "windows")
- * @library /java/awt/regtesthelpers
- * @build PassFailJFrame
  * @run main/manual TestRadioAndCheckMenuItemWithIcon
  */
-
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -50,45 +55,65 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 public class TestRadioAndCheckMenuItemWithIcon {
 
     private static final String INSTRUCTIONS =
-         "A top level Menu will be shown.\n"
-         + "\n"
-         + "Clicking on the Menu will show a\n"
-         + "JRadioButtonMenuItem group with 3 radiobutton menuitems\n"
-         + "and a JCheckBoxMenuItem group with 3 checkbox menuitems.\n"
-         + "\n"
-         + "First radiobutton menuitem is selected with imageicon of a red square.\n"
-         + "Second radiobutton menuitem is unselected with imageicon.\n"
-         + "Third radiobutton menuItem is unselected without imageicon.\n"
-         + "\n"
-         + "First checkbox menuitem is selected with imageicon.\n"
-         + "Second checkbox menuitem is unselected with imageicon.\n"
-         + "Third checkbox menuItem is unselected without imageicon.\n"
-         + "\n"
-         + "Verify that for first JRadioButtonMenuItem with imageicon,\n"
-         + "a bullet is shown alongside the imageicon and\n"
-         + "for first JCheckBoxMenuItem with imageicon\n"
-         + "a checkmark is shown alongside the imageicon.\n"
-         + "\n"
-         + "If bullet and checkmark is shown, test passes else fails.";
+            "Clicking on the Menu above will show a\n"
+            + "JRadioButtonMenuItem group with 3 radiobutton menuitems\n"
+            + "and a JCheckBoxMenuItem group with 3 checkbox menuitems.\n"
+            + "\n"
+            + "First radiobutton menuitem is selected with imageicon of a red square.\n"
+            + "Second radiobutton menuitem is unselected with imageicon.\n"
+            + "Third radiobutton menuItem is unselected without imageicon.\n"
+            + "\n"
+            + "First checkbox menuitem is selected with imageicon.\n"
+            + "Second checkbox menuitem is unselected with imageicon.\n"
+            + "Third checkbox menuItem is unselected without imageicon.\n"
+            + "\n"
+            + "Verify that for first JRadioButtonMenuItem with imageicon,\n"
+            + "a bullet is shown alongside the imageicon and\n"
+            + "for first JCheckBoxMenuItem with imageicon\n"
+            + "a checkmark is shown alongside the imageicon.\n"
+            + "\n"
+            + "If bullet and checkmark is shown, test passes else fails.";
+
+    private static final LinkedBlockingQueue<Boolean> resultQueue = new LinkedBlockingQueue<Boolean>(1);
+
+    private static void endTest(boolean result) {
+        SwingUtilities.invokeLater(
+                new Runnable() {
+                    public void run() {
+                        try {
+                            resultQueue.put(result);
+                        } catch (Exception ignored) {
+                        }
+                    }
+                });
+    }
 
     public static void main(String[] args) throws Exception {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        PassFailJFrame.builder()
-                .title("JRadioButtonMenuItem Instructions")
-                .instructions(INSTRUCTIONS)
-                .columns(60)
-                .testUI(TestRadioAndCheckMenuItemWithIcon::doTest)
-                .build()
-                .awaitAndCheck();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                TestRadioAndCheckMenuItemWithIcon.doTest();
+            }
+        });
+
+        Boolean testPasses = resultQueue.take();
+
+        if (!testPasses) {
+            throw new Exception("Test failed!");
+        }
     }
 
-    public static JFrame doTest() {
+    public static void doTest() {
         BufferedImage img = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
         Graphics g = img.getGraphics();
         g.setColor(Color.red);
@@ -107,7 +132,7 @@ public class TestRadioAndCheckMenuItemWithIcon {
         AbstractButton button1;
         JRadioButtonMenuItem m1 = new JRadioButtonMenuItem("JRadioButtonMenuItem 1",
                 imageIcon1);
-        m1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, ActionEvent.ALT_MASK|ActionEvent.CTRL_MASK|ActionEvent.SHIFT_MASK));
+        m1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, ActionEvent.ALT_MASK | ActionEvent.CTRL_MASK | ActionEvent.SHIFT_MASK));
         button1 = m1;
         button1.setSelected(true);
         AbstractButton button2 = new JRadioButtonMenuItem("JRadioButtonMenuItem 2", imageIcon2);
@@ -124,7 +149,7 @@ public class TestRadioAndCheckMenuItemWithIcon {
         AbstractButton check2 = new JCheckBoxMenuItem("JCheckBoxMenuItem 2", imageIcon1);
         JCheckBoxMenuItem c3;
         AbstractButton check3 = c3 = new JCheckBoxMenuItem("JCheckBoxMenuItem 3");
-        c3.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F5, ActionEvent.ALT_MASK|ActionEvent.CTRL_MASK|ActionEvent.SHIFT_MASK));
+        c3.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F5, ActionEvent.ALT_MASK | ActionEvent.CTRL_MASK | ActionEvent.SHIFT_MASK));
 
         JMenu topLevel = new JMenu("Menu");
 
@@ -148,8 +173,56 @@ public class TestRadioAndCheckMenuItemWithIcon {
         menuBar.add(topLevel);
 
         frame.setJMenuBar(menuBar);
-        frame.setSize(300, 300);
-        return frame;
 
+        JTextArea instructions = new JTextArea();
+        instructions.setText(INSTRUCTIONS);
+        instructions.setEditable(false);
+        instructions.setColumns(80);
+        instructions.setRows(24);
+
+        JScrollPane scrInstructions = new JScrollPane(instructions);
+        frame.getContentPane().setLayout(new BorderLayout());
+
+        frame.getContentPane().add(scrInstructions, BorderLayout.CENTER);
+
+        JPanel yesno = new JPanel();
+        yesno.setLayout(new GridLayout(1, 2, 40, 40));
+
+        JButton yes = new JButton("Passes");
+        JButton no = new JButton("Fails");
+        yesno.add(yes);
+        yesno.add(no);
+
+        yes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                endTest(true);
+            }
+        });
+
+        no.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                endTest(false);
+            }
+        });
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                frame.dispose();
+                endTest(false);
+}
+        });
+
+        frame.getContentPane().add(yesno, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
+
