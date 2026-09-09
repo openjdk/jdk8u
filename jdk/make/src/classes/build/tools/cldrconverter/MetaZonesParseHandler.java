2d692a20fe7d4bb3183bd099f1e5178f65beddec
Min Choi <minschoi@amazon.com>
2026-09-09 20:44:56 +0000

    Backport b098a8c16d5b62ca471f14e8e3f87b22e798eee3

--- jdk/make/src/classes/build/tools/cldrconverter/MetaZonesParseHandler.java
+++ jdk/make/src/classes/build/tools/cldrconverter/MetaZonesParseHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -60,6 +60,13 @@ class MetaZonesParseHandler extends AbstractLDMLHandler<String> {
             // Ignore any historical zone names (for now)
             if (attributes.getValue("to") == null) {
                 metazone = attributes.getValue("mzone");
+
+                // Explicit metazone DST offsets. Only the "dst" offset is needed,
+                // as "std" is used by default when it doesn't match.
+                String dstOffset = attributes.getValue("dstOffset");
+                if (dstOffset != null) {
+                    CLDRConverter.explicitDstOffsets.put(tzid, dstOffset);
+                }
             }
             pushIgnoredContainer(qName);
             break;
